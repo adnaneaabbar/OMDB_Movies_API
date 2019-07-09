eab5e8fa36180983bcea671d753fd121136f264b
Adnane Aabbar <44345798+parzival69@users.noreply.github.com>
2019-07-09 18:08:17 +0100

    Add files via upload

---
+++ MovieAPI.js
@@ -0,0 +1,26 @@
+//http://www.omdbapi.com/?i=tt3896198&apikey=thewdb 
+var express = require("express");
+var app = express();
+var request = require("request");
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+    res.render("search");
+});
+
+app.get("/results", function(req, res){
+    var keyword = req.query.search.toString();
+    var url = "http://www.omdbapi.com/?s=" + keyword + "&apikey=thewdb";
+    request(url, function(error, response, body){
+        if(!error & response.statusCode == 200){
+            var data = JSON.parse(body);
+            res.render("results", {data: data});  
+        }
+    });
+});
+
+app.listen("3000", function(){
+    console.log("Movie App had started on port 3000 ! ");
+});
